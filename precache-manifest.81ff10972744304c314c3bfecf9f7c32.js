18855a6b9b428df05477ae04f8e3ca637ad16d79
thedevsjourney <thedevsjourney@gmail.com>
2019-04-22 22:34:19 -0500

    Updates

---
+++ precache-manifest.81ff10972744304c314c3bfecf9f7c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b1ebe4e59719d77cb7b",
+    "url": "/TheDevsJourney/CloudFlick.git/static/js/runtime~main.e89a0c9b.js"
+  },
+  {
+    "revision": "b23a84ce7f3155925f87",
+    "url": "/TheDevsJourney/CloudFlick.git/static/js/main.2b109018.chunk.js"
+  },
+  {
+    "revision": "19987ee9b83934397e45",
+    "url": "/TheDevsJourney/CloudFlick.git/static/js/2.b72f4ae1.chunk.js"
+  },
+  {
+    "revision": "b23a84ce7f3155925f87",
+    "url": "/TheDevsJourney/CloudFlick.git/static/css/main.2d72e6a1.chunk.css"
+  },
+  {
+    "revision": "9e27c787ae36d6517173261b217bcbb3",
+    "url": "/TheDevsJourney/CloudFlick.git/index.html"
+  }
+];
