16d2eda3abfc4bceb605b90ac61f78adf058b51b
thedevsjourney <thedevsjourney@gmail.com>
2019-04-22 22:50:52 -0500

    Updates

---
+++ precache-manifest.a6a0724c09632e36260a27fefffee0bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3acf1e0a383c88b36b36",
+    "url": "/CloudFlick/static/js/runtime~main.6b2174f0.js"
+  },
+  {
+    "revision": "ddc26817ebad8ece5a54",
+    "url": "/CloudFlick/static/js/main.09f7165e.chunk.js"
+  },
+  {
+    "revision": "a5a385840404928b73fc",
+    "url": "/CloudFlick/static/js/2.d2b727d2.chunk.js"
+  },
+  {
+    "revision": "ddc26817ebad8ece5a54",
+    "url": "/CloudFlick/static/css/main.2d72e6a1.chunk.css"
+  },
+  {
+    "revision": "fadccf29da270d917a5e50ab3d9a3889",
+    "url": "/CloudFlick/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TheDevsJourney/CloudFlick.git/precache-manifest.a3826343f9de780137e44905dab13c6f.js"
+  "/CloudFlick/precache-manifest.a6a0724c09632e36260a27fefffee0bd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TheDevsJourney/CloudFlick.git/index.html", {
+workbox.routing.registerNavigationRoute("/CloudFlick/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
